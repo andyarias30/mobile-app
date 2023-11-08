1661a423e6472ab42d0715e8d4b425d22d2d4e6e
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-08 13:32:34 -0500

    blogapp react native

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet, Text, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import PostCard from './components/PostCard';
 
 export default function App() {
+  const [blogPost, setBlogPost] = useState([])
+  // useeffect is being called on different lifecycles of the components
+  //1. When components Mounts
+  //2. when components unMounts
+
+  useEffect(() => {
+    fetch('http://192.168.10.98:8080')
+      .then(res => res.json())
+      .then(data => setBlogPost(data))
+      .catch(err => console.error(err))
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View>
+      <ScrollView>
+        {blogPost.map((singlePost, index) => {
+          return(
+            <PostCard singlePost={singlePost} index={index} key={singlePost._id} />
+          )
+        })}
+      </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      // display: 'flex',
+      // height: '100%',
+      // width: '400',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
   },
-});
+})
